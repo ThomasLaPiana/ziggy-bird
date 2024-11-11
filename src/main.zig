8fcbf60d8719344e9816d66321db2dde8929ee07
Thomas La Piana <thomas@aampe.com>
2024-11-11 22:59:59 +0800

    add game over logic

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const rl = @import("raylib");
 
 // Set all of the Game constants
 const birdSize: i16 = 50;
-const fps: i32 = 60;
+const fps: i32 = 120;
 const screenWidth: i32 = 1000;
 const screenHeight: i32 = 700;
 const fallRate: f32 = 1.0;
@@ -18,6 +18,7 @@ pub fn main() anyerror!void {
     defer rl.closeWindow();
 
     var birdPosition = rl.Vector2.init(screenWidth / 2, screenHeight / 2);
+    var gameLost = false;
 
     // Get a pair of obstacles with a gap in the middle
     const first = obs.get_obstacle_pair(screenWidth, screenHeight);
@@ -45,20 +46,11 @@ pub fn main() anyerror!void {
 
         // Update
         //----------------------------------------------------------------------------------
-
-        // Move the Bird
-        birdPosition.y += fallRate;
-
-        // Move the Columns
-        for (&obstacles) |*pair| {
-            pair.move_left(scrollRate);
+        if (!gameLost) {
+            const key_pressed = rl.isKeyPressed(rl.KeyboardKey.key_k);
+            move_objects(&birdPosition, &obstacles, &camera, key_pressed);
         }
 
-        camera.target = rl.Vector2.init(birdPosition.x + 20, screenHeight / 2);
-
-        if (rl.isKeyPressed(rl.KeyboardKey.key_k)) {
-            birdPosition.y -= 50.0;
-        }
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -82,15 +74,54 @@ pub fn main() anyerror!void {
                 rl.drawRectangleRec(pair.bottom, rl.Color.dark_blue);
             }
         }
-        // Check if we passed an obstacle
+
+        // Calculate score and collisions
         for (obstacles) |pair| {
+            const topCollision = rl.checkCollisionCircleRec(birdPosition, birdSize, pair.top);
+            const bottomCollision = rl.checkCollisionCircleRec(birdPosition, birdSize, pair.bottom);
+            if (topCollision or bottomCollision) {
+                gameLost = true;
+                continue;
+            }
+
             if (pair.top.x == screenWidth / 2) {
                 score += 1;
             }
         }
 
+        if (gameLost) {
+            rl.drawText("Game Over!", screenWidth / 2 - 100, screenHeight / 2, 20, rl.Color.black);
+        }
+
+        // Draw the Score
         const scoreText = rl.textFormat("Score: %d", .{score});
         rl.drawText(scoreText, 10, 10, 20, rl.Color.dark_gray);
+
+        // Draw the Time
+        const timeText = rl.textFormat("Elapsed Time: %f", .{rl.getTime()});
+        rl.drawText(timeText, 10, 30, 20, rl.Color.dark_gray);
+
+        // Draw the FPS
+        const fpsText = rl.textFormat("Frames/Second: %d", .{rl.getFPS()});
+        rl.drawText(fpsText, 10, 50, 20, rl.Color.dark_gray);
+
         //----------------------------------------------------------------------------------
+    }
+}
+
+// Move all of the Objects
+pub fn move_objects(birdPosition: *rl.Vector2, obstacles: *[5]obs.ObstaclePair, camera: *rl.Camera2D, key_pressed: bool) void {
+    // Move the Bird
+    birdPosition.y += fallRate;
+
+    // Move the Columns
+    for (obstacles) |*pair| {
+        pair.move_left(scrollRate);
+    }
+
+    camera.target = rl.Vector2.init(birdPosition.x + 20, screenHeight / 2);
+
+    if (key_pressed) {
+        birdPosition.y -= 50.0;
     }
 }
